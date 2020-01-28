6daaf62a866648760eca245537ed6b9e1d001928
danielthestripedtiger <60051207+danielthestripedtiger@users.noreply.github.com>
2020-01-28 16:18:37 -0600

    Commented out unneeded contract functions (but keeping them as comments for future reference)

--- src/contracts/DocStoreV2.sol
+++ src/contracts/DocStoreV2.sol
@@ -91,40 +91,40 @@ contract DocStore
             );
     }
 
-    function stringToBytes32(string memory source) private returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
+    // function stringToBytes32(string memory source) private returns (bytes32 result) {
+    //     bytes memory tempEmptyStringTest = bytes(source);
         
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
+    //     if (tempEmptyStringTest.length == 0) {
+    //         return 0x0;
+    //     }
 
-        assembly {
-            result := mload(add(source, 32))
-        }
-    }
+    //     assembly {
+    //         result := mload(add(source, 32))
+    //     }
+    // }
     
-    function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) internal returns (string memory){
-    bytes memory _ba = bytes(_a);
-    bytes memory _bb = bytes(_b);
-    bytes memory _bc = bytes(_c);
-    bytes memory _bd = bytes(_d);
-    bytes memory _be = bytes(_e);
-    string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-    bytes memory babcde = bytes(abcde);
-    uint k = 0;
-    for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-    for (uint i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-    for (uint i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-    for (uint i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-    for (uint i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-    return string(babcde);
-}
+//     function strConcat(string memory _a, string memory _b, string memory _c, string memory _d, string memory _e) internal returns (string memory){
+//     bytes memory _ba = bytes(_a);
+//     bytes memory _bb = bytes(_b);
+//     bytes memory _bc = bytes(_c);
+//     bytes memory _bd = bytes(_d);
+//     bytes memory _be = bytes(_e);
+//     string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
+//     bytes memory babcde = bytes(abcde);
+//     uint k = 0;
+//     for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+//     for (uint i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+//     for (uint i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
+//     for (uint i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
+//     for (uint i = 0; i < _be.length; i++) babcde[k++] = _be[i];
+//     return string(babcde);
+// }
 
 
 
-function strConcat(string memory _a, string memory _b) internal returns (string memory) {
-    return strConcat(_a, _b, "", "", "");
-}
+// function strConcat(string memory _a, string memory _b) internal returns (string memory) {
+//     return strConcat(_a, _b, "", "", "");
+// }
 
 }
 
