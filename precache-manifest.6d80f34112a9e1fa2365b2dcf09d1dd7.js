3adf3e432334e189da7986f68ddbe6c29cc59bee
danielthestripedtiger <60051207+danielthestripedtiger@users.noreply.github.com>
2020-01-29 14:36:50 -0600

    Updates

---
+++ precache-manifest.6d80f34112a9e1fa2365b2dcf09d1dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d92ab81ce81d300ae46402b35f865ea",
+    "url": "/attester/index.html"
+  },
+  {
+    "revision": "0f3c47bf8299e982fb82",
+    "url": "/attester/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7aaea56ea8bc4fb386a8",
+    "url": "/attester/static/js/2.145a4fbe.chunk.js"
+  },
+  {
+    "revision": "ab52121242a155d5610194a440e1df20",
+    "url": "/attester/static/js/2.145a4fbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f3c47bf8299e982fb82",
+    "url": "/attester/static/js/main.f8b05afb.chunk.js"
+  },
+  {
+    "revision": "044dea36307778d21719",
+    "url": "/attester/static/js/runtime-main.6c44be78.js"
+  }
+]);
